d6eb37cb2f5d52d8cdaf2140b806fa7408a9def6
Rishikesh <rishikksh20@gmail.com>
2015-11-26 08:35:30 +0000

    Adding a setup screen

---
+++ src/com/rishi/nuc/mainApp.java
@@ -0,0 +1,16 @@
+package com.rishi.nuc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class mainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
